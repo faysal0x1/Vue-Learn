21ad695cc113e2587f7b58e8a01487a16ec9db8f
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2024-09-16 08:46:56 +0600

    router push

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
-        <h1>Service Page
-
+        <h1>
+            Service Page
         </h1>
+
+        <button @click="goHome">Go to Home page</button>
     </div>
 </template>
 
 <script setup>
-
+import { useRouter } from 'vue-router';
+const router = useRouter()
+const goHome=()=>{
+    router.push({name:'home'})
+}
 </script>
 
 <style scoped></style>
